d9222451d9005ddc742829b3d2b105849e9ac76a
ANDRES_MIGUEL_RIVERA_FRANCISCO <a.22308051281295@cbtis128.edu.mx>
2025-02-27 02:24:25 +0000

    Practica 9 Icons V2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,133 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyIconsAPP());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyIconsAPP extends StatelessWidget {
+  const MyIconsAPP({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Andres Rivera | 22308051281295'),
+          centerTitle: true,
+          backgroundColor: Color(0xff460840),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(
+              'Andres Rivera | 22308051281295',
+              style: TextStyle(color: Color(0xffa80000), fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                // Ícono 1 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_a_photo,
+                      color: Colors.pink,
+                      size: 24.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'Cámara',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                // Ícono 2 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.backpack,
+                      color: Colors.green,
+                      size: 30.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'Mochila',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                // Ícono 3 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.blue,
+                      size: 36.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'Carrito',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                // Ícono 1 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.tiktok,
+                      color: Color(0xff000000),
+                      size: 24.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'TikTok',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                // Ícono 2 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.facebook,
+                      color: Color(0xff006aff),
+                      size: 30.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'Facebook',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                // Ícono 3 con subtítulo
+                Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Color(0xffff0000),
+                      size: 36.0,
+                    ),
+                    SizedBox(height: 8), // Espacio entre ícono y texto
+                    Text(
+                      'Mail',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } //end widgets
+} //end class MyIconsApp
